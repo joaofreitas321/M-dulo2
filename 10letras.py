d1be82a5241335025992ba253cd2c0cb8f896a67
João Freitas <a25077@esenviseu.net>
2024-10-28 16:32:47 +0000

    Exercicio 10 letras

--- 10letras.py
+++ 10letras.py
@@ -6,6 +6,10 @@ deve indicar quantas vogais o utilizador introduziu.
 contar = 0
 for i in range(10):
     letra = input("Introduza uma letra")
-    if letra in "aeiouAEIOU":
-        contar = contar + 1
+    letra = letra.strip()
+    if len(letra)!=1:
+        print("Só pode inserir uma letra")
+    else:
+        if letra in "aeiouAEIOU":
+            contar = contar + 1
 print(contar)        
