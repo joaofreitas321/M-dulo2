f46da974fdf3e90cc247e3ed8162087743d91d6f
João Freitas <a25077@esenviseu.net>
2024-11-17 15:23:46 +0000

    desafio condicional 2

---
+++ desafio_condicional2.py
@@ -0,0 +1,7 @@
+# Programa qeu diz se o aluno fica aprovado ou não consoante a sua nota
+nota_aluno = int(input("Introduza a nota do aluno"))
+
+if nota_aluno >= 10:
+    print("Aprovado")
+else:
+    print("Não aprovado")    
