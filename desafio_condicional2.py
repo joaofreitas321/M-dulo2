a1afff1a816d0502ed430efc1f6299881135f316
João Freitas <a25077@esenviseu.net>
2024-11-18 12:27:18 +0000

    desafio condicional 3

--- desafio_condicional2.py
+++ desafio_condicional2.py
@@ -1,4 +1,4 @@
-# Programa qeu diz se o aluno fica aprovado ou não consoante a sua nota
+# Programa que diz se o aluno fica aprovado ou não consoante a sua nota
 nota_aluno = int(input("Introduza a nota do aluno"))
 
 if nota_aluno >= 10:
--- supermercado.py
+++ supermercado.py
@@ -1,16 +1,11 @@
 # Programa que pergunta quantas unidades o cliente comprou e calcule o preço total a pagar
-preco_unitario = 10
-preco_total = 0
-unidades = 0
+unidades = int(input("Introduza o numero de unidades que comprou"))
+quantidade = int(input("Introduza a quantidade"))
 
-unidades_compradas = int(input("Quantas unidades comprou"))
-
-while unidades_compradas < unidades:
-    unidades_compradas = unidades_compradas + 1
-
-    if unidades_compradas <= 10:
-        preco_total = preco_total + preco_unitario
-    elif unidades_compradas <= 20:
-        preco_total = preco_total + preco_unitario * 0.95
-    else:
-        preco_total = preco_total + preco_unitario * 0.90
+while unidades <= 10:
+    valor = 10
+while unidades > 10 and unidades <=20:
+    valor = 10 * 0.05 * quantidade
+while unidades > 20:
+    valor = 10 * 0.20 * quantidade
+print(valor)         
